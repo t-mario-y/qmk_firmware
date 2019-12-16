c52329663616c219c4eebaea950118bf2600b300
t-mario-y <t.mario.yoshi@gmail.com>
2019-12-17 01:41:10 +0900

    英数/かなキーおよび、HJKL arrowを追加

--- keyboards/ergodash/rev1/keymaps/t-mario-y/keymap.c
+++ keyboards/ergodash/rev1/keymaps/t-mario-y/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Del  |      | Bspc |      |   N  |   M  |   ,  |   .  |  Up  | Shift|
    * |-------------+------+------+------+------+ Space+------+------+------+ Enter+------+------+------+------+-------------|
-   * | Caps | Lower|OptAlt| GUI  |||||||| Lower|      | Del  |||||||| Bspc |      | Raise|||||||| Raise| Left | Down | Right|
+   * | Caps | Lower|OptAlt| GUI  |||||||| Lower|      | Eisu |||||||| Kana |      | Raise|||||||| Raise| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_SLSH, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_DEL,        KC_BSPC,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT, \
-    KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_DEL,        KC_BSPC, KC_ENT,  RAISE,            RAISE,   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_LANG2,      KC_LANG1,KC_ENT,  RAISE,            RAISE,   KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Lower(数字→Functionキー、右上BackSpace→Delete、QWERTY行に1~0のShift時の入力をスライドさせる。それ以外は変えない)
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Del  |      | Bspc |      |   N  |   M  |   ,  |   .  |  Up  | Shift|
    * |-------------+------+------+------+------+ Space+------+------+------+ Enter+------+------+------+------+-------------|
-   * | Caps | Lower|OptAlt| GUI  |||||||| Lower|      | Del  |||||||| Bspc |      | Raise|||||||| Raise| Left | Down | Right|
+   * | Caps | Lower|OptAlt| GUI  |||||||| Lower|      | Eisu |||||||| Kana |      | Raise|||||||| Raise| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
@@ -57,28 +57,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_MINS,                        KC_EQL,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_SLSH, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_DEL,        KC_BSPC,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT, \
-    KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_DEL,        KC_BSPC, KC_ENT,  RAISE,            RAISE,   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_LANG2,      KC_LANG1,KC_ENT,  RAISE,            RAISE,   KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
-  /* Raise(Lowerと同じ)
+  /* Raise(HJKL arrow)
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   [  |                    |   ]  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   !  |   @  |   #  |   $  |   %  |   -  |                    |   =  |   ^  |   &  |   *  |   (  |   )  | Del  |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |   \  |                    |   /  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |   \  |                    |   /  | LEFT | DOWN |  UP  | RIGHT|   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Del  |      | Bspc |      |   N  |   M  |   ,  |   .  |  Up  | Shift|
    * |-------------+------+------+------+------+ Space+------+------+------+ Enter+------+------+------+------+-------------|
-   * | Caps | Lower|OptAlt| GUI  |||||||| Lower|      | Del  |||||||| Bspc |      | Raise|||||||| Raise| Left | Down | Right|
+   * | Caps | Lower|OptAlt| GUI  |||||||| Lower|      | Eisu |||||||| Kana |      | Raise|||||||| Raise| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LBRC,                        KC_RBRC, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_MINS,                        KC_EQL,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_SLSH, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_SLSH, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_DEL,        KC_BSPC,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT, \
-    KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_DEL,        KC_BSPC, KC_ENT,  RAISE,            RAISE,   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_LANG2,      KC_LANG1,KC_ENT,  RAISE,            RAISE,   KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Adjust
