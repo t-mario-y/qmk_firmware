dc6b1e84c9af4bc553c2b7156cb73504b4d99e02
t-mario-y <t.mario.yoshi@gmail.com>
2020-01-29 01:50:31 +0900

    数字キーを4列に収める

--- keyboards/ergodash/rev1/keymaps/t-mario-y/keymap.c
+++ keyboards/ergodash/rev1/keymaps/t-mario-y/keymap.c
@@ -38,11 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_LANG2,      KC_LANG1,KC_ENT,  RAISE,            KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
-  /* Lower(数字→Functionキー、右上BackSpace→Delete、QWERTY行に1~0のShift時の入力をスライドさせる。それ以外は変えない)
+  /* Lower
+   * 数字→Functionキー
+   * 右上BackSpace→Delete
+   * QWERTY行に1~0の数字を割り当て
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   [  |                    |   ]  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   !  |   @  |   #  |   $  |   %  |   -  |                    |   =  |   ^  |   &  |   *  |   (  |   )  | Del  |
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |   -  |                    |   =  |   6  |   7  |   8  |   9  |   0  | Del  |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |   \  |                    |   /  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
@@ -53,13 +56,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LBRC,                        KC_RBRC, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_MINS,                        KC_EQL,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_SLSH, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_DEL,        KC_BSPC,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT, \
     KC_CAPS, LOWER,   KC_LALT, KC_LGUI,          LOWER,   KC_SPC, KC_LANG2,      KC_LANG1,KC_ENT,  RAISE,            KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
-  /* Raise(HJKL arrow)
+  /* Raise
+   * 数字→Functionキー
+   * 右上BackSpace→Delete
+   * QWERTY行に1~0のShift時の入力記号をスライドさせる
+   * HJKL arrow
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   [  |                    |   ]  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
